3b1d9de758ba0b4b233f390cc165bbcc4dfcab8a
Elliot Winard <elliot.winard@frogdesign.com>
2013-12-10 16:16:46 -0500

    listen for stream of viewer events

--- test.js
+++ test.js
@@ -1,9 +1,28 @@
-var naim = require('./lib/naim');
+var naim =require('./lib/naim');
+
 naim.connect(null,null,function(err) {
     console.log('connected');
 
-    naim.getViewerCount(function(err, count) {
-        if (err) return err;
-        console.log("got viewer count of", count);
+    // simple calls
+    naim.getAudienceCount(function(err, count) {
+        if (err) { console.log('ERR',err); return err; }
+        console.log('got viewer count of', count);
       });
+
+    naim.getAudienceDetails(function(err, details) {
+        if (err) { console.log('ERR',err); return err; }
+        console.log('got audience details', details);
+      });
+    
+    // stream
+    naim.listen(true);
+    naim.on('ACK', function() {
+        console.log('ACK received');
+    });
+    naim.on('NACK', function() {
+        console.log('NACK received');
+    });
+    naim.on('EVENT_VIEWER', function(d) {
+        console.log('EVENT_VIEWER received',d);
+    });
   })
