eec1164e276f5f8124f9168577b431540a7b98e6
Elliot Winard <elliot.winard@frogdesign.com>
2013-12-11 10:31:42 -0500

    call remote_action

--- test/ping_aim.js
+++ test/ping_aim.js
@@ -1,18 +1,31 @@
-var naim =require('./lib/naim');
-var MS_BETWEEN_POLLS = 1000;
+var naim = require('../lib/naim'),
+    MS_BETWEEN_POLLS = 1000,
+    path = require('path'),
+    isWin = !!process.platform.match(/^win/),
+    BSBIN = (isWin)?'C:\Program Files (x86)\BroadSign\bsp\bin':'/opt/broadsign/suite/bsp/bin',
+    ENABLE_CMD = BSBIN + path.sep + 'remote_action' + (isWin?'.exe':'')+ ' condition -e 1 -n "Group"',
+    DISABLE_CMD = BSBIN + path.sep + 'remote_action' + (isWin?'.exe':'') + ' condition -e 0 -n "Group"',
+    HOST = 'think-a',
+    exec = require('child_process').exec;
+    
 
-naim.connect(null,null,function(err) {
+naim.connect(HOST,null,function(err) {
     console.log('connected');
     // poll
-    function pollForDetails() {
-        var enable='remote_action.exe condition -e 1 -n "Group"',
-            disable = 'remote_action.exe condition -e 0 -n "Group"';
-        
+    function pollForDetails() {        
         naim.getAudienceDetails(function (err, details) {
             if (err) { console.log('ERR',err); return err; }
             
-            var cmd = (details.length>1)?enable:disable;
-            console.log('call',details.length,cmd);
+            var cmd = (details.length>1)?ENABLE_CMD:DISABLE_CMD;
+            console.log('calling',details.length,cmd);
+            exec(cmd,  
+                function (error, stdout, stderr) {
+                    if(stdout) console.log('stdout: ' + stdout);
+                    if(stderr)console.log('stderr: ' + stderr);
+                    if (error !== null) {
+                        console.log('exec error: ' + error);
+                    }
+                });
         });
     };
     setInterval(pollForDetails, MS_BETWEEN_POLLS);
